6d6cc321a2ac26ba337c130a4909e9c0387590ec
kevinnaserwan <kevinnaserwan@gmail.com>
2024-04-08 23:04:36 +0700

    Feat: carousel

--- src/app/sejarah/components/carousel.tsx
+++ src/app/sejarah/components/carousel.tsx
@@ -33,6 +33,7 @@ export function CarouselDemo() {
               year={2023}
               image="/assets/images/ketua-1.svg"
               hidden=""
+              hidden2=""
               width="50"
             />
           </div>
@@ -45,6 +46,7 @@ export function CarouselDemo() {
               year={2024}
               image="/assets/images/ketua-1.svg"
               hidden="hidden"
+              hidden2="hidden"
               width="70"
             />
           </div>
