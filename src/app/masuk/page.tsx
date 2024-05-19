63bcb53283c116d4a0e4c71070c0a1859bcd0bdd
kevinnaserwan <kevinnaserwan@gmail.com>
2024-05-19 16:22:34 +0700

    Feat: setup and consume login api service

--- src/app/masuk/page.tsx
+++ src/app/masuk/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useEffect, useState, Fragment, Suspense } from "react";
+import { useEffect, useState, Fragment, Suspense, FormEvent } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@/components/form";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,10 +14,10 @@ interface Errors {
 }
 function LoginContent() {
   const [email, setEmail] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [errors, setErrors] = useState<Errors>({});
+  const [apiError, setApiError] = useState<string | null>(null);
+  const router = useRouter();
 
   const validateForm = (): Errors => {
     const errors: Errors = {};
@@ -37,52 +37,68 @@ function LoginContent() {
     return errors;
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const validationErrors = validateForm();
     if (Object.keys(validationErrors).length === 0) {
-      // Continue with form submission
-      console.log("Form submitted");
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/v1/user/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+          }
+        );
+        const data = await response.json();
+        if (response.ok) {
+          localStorage.setItem("token", data.value.token);
+          router.push("/");
+        } else {
+          setApiError(data.message);
+        }
+      } catch (error) {
+        setApiError("An error occurred. Please try again.");
+      }
     } else {
       setErrors(validationErrors);
     }
   };
+
   return (
     <main className="overflow-hidden lg:bg-none bg-login-hero-image-mobile bg-cover bg-center max-h-screen">
-      <div className=" lg:flex lg:items-center overflow-hidden pt-[25%] pb-[50%] lg:mt-0 lg:py-0">
-        <div className=" hidden lg:block">
+      <div className="lg:flex lg:items-center overflow-hidden pt-[25%] pb-[50%] lg:mt-0 lg:py-0">
+        <div className="hidden lg:block">
           <Image
             src="/assets/images/login-image.webp"
             alt=""
             width={700}
             height={700}
-            className=" max-h-screen "
+            className="max-h-screen"
           />
         </div>
-        <div className=" bg-white/80 lg:max-w-full max-w-[320px] mx-auto lg:py-0 lg:px-0 py-5 px-8 rounded-[20px]">
-          <div className=" lg:max-w-full max-w-[300px] mx-auto lg:mb-12 pb-5 lg:pb-0">
-            <div className=" mx-auto">
+        <div className="bg-white/80 lg:max-w-full max-w-[320px] mx-auto lg:py-0 lg:px-0 py-5 px-8 rounded-[20px]">
+          <div className="lg:max-w-full max-w-[300px] mx-auto lg:mb-12 pb-5 lg:pb-0">
+            <div className="mx-auto">
               <Image
                 src="/assets/icon/icon.svg"
                 width={100}
                 height={100}
                 alt=""
-                className=" mx-auto lg:m-0 lg:mx- lg:w-[100px] lg:h-[100px] w-[70px] h-[70px]"
+                className="mx-auto lg:m-0 lg:mx- lg:w-[100px] lg:h-[100px] w-[70px] h-[70px]"
               />
-              <div className=" h-[2px] bg-[#D9D9D9] lg:block hidden"></div>
-              <div className=" mt-2 lg:mt-12">
-                <h1 className=" lg:font-bold lg:text-4xl font-semibold text-2xl">
+              <div className="h-[2px] bg-[#D9D9D9] lg:block hidden"></div>
+              <div className="mt-2 lg:mt-12">
+                <h1 className="lg:font-bold lg:text-4xl font-semibold text-2xl">
                   Masuk ke Akun Anda!
                 </h1>
-                <p className=" lg:font-medium lg:text-sm font-semibold text-xs text-[#414141] mt-1">
+                <p className="lg:font-medium lg:text-sm font-semibold text-xs text-[#414141] mt-1">
                   Selamat datang, silahkan masukkan email dan kata sandi Anda.
                 </p>
               </div>
-              <form
-                action=""
-                onSubmit={handleSubmit}
-                className=" = lg:my-12 my-5"
-              >
+              <form action="" onSubmit={handleSubmit} className="lg:my-12 my-5">
                 <Form
                   htmlFor="email"
                   label="Email"
@@ -109,39 +125,40 @@ function LoginContent() {
                 {errors.password && (
                   <p className="text-red-500 text-xs">{errors.password}</p>
                 )}
-                <div className=" flex justify-between items-center mb-5 mt-2">
-                  <div className=" flex gap-2">
+                {apiError && <p className="text-red-500 text-xs">{apiError}</p>}
+                <div className="flex justify-between items-center mb-5 mt-2">
+                  <div className="flex gap-2">
                     <input
                       type="checkbox"
-                      className=" checked:bg-blue-color"
+                      className="checked:bg-blue-color"
                       id="remember"
                     />
                     <label
                       htmlFor="remember"
-                      className=" text-xs font-normal text-[#979797]"
+                      className="text-xs font-normal text-[#979797]"
                     >
                       Ingatkan saya
                     </label>
                   </div>
                   <Link
                     href=""
-                    className=" font-bold text-xs text-blue-color hover:underline"
+                    className="font-bold text-xs text-blue-color hover:underline"
                   >
                     Lupa Password?
                   </Link>
                 </div>
-                <button className=" w-full bg-blue-color rounded-lg hover:bg-dark-color">
-                  <p className=" font-bold text-white text-sm py-3">Masuk</p>
+                <button className="w-full bg-blue-color rounded-lg hover:bg-dark-color">
+                  <p className="font-bold text-white text-sm py-3">Masuk</p>
                 </button>
               </form>
-              <div className=" h-[2px] bg-[#D9D9D9] lg:block hidden"></div>
-              <div className=" flex lg:gap-1 gap-1 justify-center mt-3 items-center">
-                <p className=" font-medium text-xs lg:text-sm">
+              <div className="h-[2px] bg-[#D9D9D9] lg:block hidden"></div>
+              <div className="flex lg:gap-1 gap-1 justify-center mt-3 items-center">
+                <p className="font-medium text-xs lg:text-sm">
                   Tidak Punya Akun?
                 </p>
                 <Link
                   href="/daftar"
-                  className=" lg:font-medium font-bold hover:underline text-sm text-blue-color"
+                  className="lg:font-medium font-bold hover:underline text-sm text-blue-color"
                 >
                   Daftar
                 </Link>
